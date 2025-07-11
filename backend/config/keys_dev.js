bb11a597d850320b22fc25ed20bb0ec7f6ea718e
yaroslav <maheyyaroslav2006@gmail.com>
2025-07-11 12:39:03 +0300

    FitForge Backend

--- backend/config/keys_dev.js
+++ backend/config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'Сюда копируем URI для доступа в БД, например: mongodb+srv://username:password@..........................',
+    'mongodb+srv://FitForge:tlSASVfijzUBZwaV@fitforge.rw1obgt.mongodb.net/',
   secretOrKey: 'random very very secret string',
 };
